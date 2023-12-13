33c21c9db079ff290b9832a47f3a3a9780338266
juansesu3 <juanseu3@hotmail.com>
2023-12-13 11:54:33 +0100

    Initial Commit

---
+++ SwiftUI-Wheather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Wheather
+//
+//  Created by Juan Suarez on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Wheather/SwiftUI_WheatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WheatherApp.swift
+//  SwiftUI-Wheather
+//
+//  Created by Juan Suarez on 13/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WheatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
