2902b3cb1d8c6b441feb87fbf80b6848b99b6d9d
juansesu3 <juanseu3@hotmail.com>
2023-12-13 14:33:04 +0100

    FEATURE( SF Symbols)

--- SwiftUI-Wheather/ContentView.swift
+++ SwiftUI-Wheather/ContentView.swift
@@ -15,6 +15,22 @@ struct ContentView: View {
              .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             VStack{
                 Text("Friburgo, CH")
+                    .font(.system(size:32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing:10){
+                    Image(systemName: "cloud.sun.rain.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                }
+                Spacer()
+                   
             }
         }
     }
