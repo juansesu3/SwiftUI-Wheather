a70d75fdba55df21ed7a825698fb284a61557b7b
juansesu3 <juanseu3@hotmail.com>
2024-01-01 19:01:06 +0100

    FEATURE(progress)

--- SwiftUI-Wheather/ContentView.swift
+++ SwiftUI-Wheather/ContentView.swift
@@ -10,27 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .topLeading,
-                endPoint: .bottomTrailing)
-             .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            BackgroundView(topColor: .blue, bottomColor: Color(.lightBlue))
             VStack{
-                Text("Friburgo, CH")
-                    .font(.system(size:32, weight: .medium, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing:10){
-                    Image(systemName: "cloud.sun.rain.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(width: 180, height: 180)
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                    
+                CityTextView(cityName: "Friburg, CH")
+                MainWeatherView(imageName: "cloud.sun.fill", temperature: 5)
+                HStack(spacing:20){
+                    WeatherDayView(dayOfWeek:"TUE",
+                                   imageName:"cloud.hail.fill",
+                                   temperature: 2)
+                    WeatherDayView(dayOfWeek:"WED",
+                                   imageName:"cloud.snow.fill",
+                                   temperature: 3)
+                    WeatherDayView(dayOfWeek:"THU",
+                                   imageName:"wind.snow",
+                                   temperature: 1)
+                    WeatherDayView(dayOfWeek:"FRI",
+                                   imageName:"cloud.sun.fill",
+                                   temperature: 5)
+                    WeatherDayView(dayOfWeek:"SAT",
+                                   imageName:"snow",
+                                   temperature: 0)
                 }
                 Spacer()
-                   
+                Button{
+                    print("tapped")
+                } label:{
+                    WeatherButton(title: "Change Day Time", 
+                                  textColor: .blue,
+                                  backgroundColor:.white)
+                }
+                Spacer()
             }
         }
     }
@@ -39,4 +48,74 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+struct WeatherDayView: View {
+    
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    var body: some View {
+        
+        VStack(){
+            Text(dayOfWeek).font(.system(size:23, weight: .medium, design: .default))
+                .foregroundColor(.white)
+            VStack(spacing:10){
+                Image(systemName: imageName)
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 40, height: 40)
+                Text("\(temperature)°")
+                    .font(.system(size: 23, weight: .medium))
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var topColor: Color
+    var bottomColor: Color
+    
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading,
+            endPoint: .bottomTrailing)
+        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CityTextView: View{
+    
+    var cityName: String
+    
+    var body: some View{
+        Text(cityName)
+            .font(.system(size:23, weight: .medium, design: .default))
+                .foregroundColor(.white)
+                .padding()
+    }
+}
+
+struct MainWeatherView: View {
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing:10){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(width: 180, height: 180)
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom, 40)
+    }
+    
+}
+
 
--- SwiftUI-Wheather/WeatherButton.swift
+++ SwiftUI-Wheather/WeatherButton.swift
@@ -4,5 +4,19 @@
 //
 //  Created by Juan Suarez on 29/12/23.
 //
+import SwiftUI
 
-import Foundation
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View{
+        Text(title)
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size:20, weight:.bold, design:.default))
+            .cornerRadius(10)
+    }
+}
