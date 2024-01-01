06bf08c319b3b46cfbb1f8ba6ab69d2c1e545429
juansesu3 <juanseu3@hotmail.com>
2024-01-01 19:32:15 +0100

    FEATURE(I was finished with my first swift app)

--- SwiftUI-Wheather/ContentView.swift
+++ SwiftUI-Wheather/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isNight = false
+    
     var body: some View {
         ZStack {
-            BackgroundView(topColor: .blue, bottomColor: Color(.lightBlue))
+            BackgroundView(isNight: $isNight)
             VStack{
                 CityTextView(cityName: "Friburg, CH")
-                MainWeatherView(imageName: "cloud.sun.fill", temperature: 5)
+                MainWeatherView(imageName: isNight ? "moon.stars.fill" : "cloud.sun.fill", temperature: 5)
                 HStack(spacing:20){
                     WeatherDayView(dayOfWeek:"TUE",
                                    imageName:"cloud.hail.fill",
@@ -33,7 +36,7 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button{
-                    print("tapped")
+                    isNight.toggle()
                 } label:{
                     WeatherButton(title: "Change Day Time", 
                                   textColor: .blue,
@@ -75,11 +78,13 @@ struct WeatherDayView: View {
 
 struct BackgroundView: View {
     
-    var topColor: Color
-    var bottomColor: Color
+  
+    
+    @Binding var isNight: Bool
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [topColor, bottomColor]), startPoint: .topLeading,
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black: .blue, 
+                                                   isNight ? .gray : Color(.lightBlue)]), startPoint: .topLeading,
             endPoint: .bottomTrailing)
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
     }
